de917d8eb7fb9e71c1d37e578a7810aa2d7ae59f
WLLS253 <253540651@qq.com>
2019-07-16 19:58:01 +0800

    second

--- src/main/java/com/example/demo/repository/MissionRepository.java
+++ src/main/java/com/example/demo/repository/MissionRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MissionRepository extends JpaRepository<MissionRepository,Long> {
+public interface MissionRepository extends JpaRepository<Mission,Long> {
 }
--- src/main/java/com/example/demo/repository/TUserRepository.java
+++ src/main/java/com/example/demo/repository/TUserRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.TUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TUserRepository extends JpaRepository<TUserRepository,Long> {
+public interface TUserRepository extends JpaRepository<TUser,Long> {
 }
